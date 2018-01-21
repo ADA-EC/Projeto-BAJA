2de4fa5c86fc1f919d3035e0b30f4b6af94afd53
Amador Neto <amador.neto@usp.br>
2018-01-21 00:27:41 -0200

    Estado do projeto no dia 21/01, 0:30

--- Código/LeitorSerial.py
+++ Código/LeitorSerial.py
@@ -1,10 +1,56 @@
-from ReceptorSerialGeral import ReceptorSerialGeral
+import serial
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from time import sleep
 
-class LeitorSerial(ReceptorSerialGeral):
+class LeitorSerial():
+	
+	tstart = datetime.now()
+	
+	# cria o DataFrame com as colunas que serão preenchidas
+	col_labels = ('Tempo',
+          'Box',
+          'Velocidade',
+          'Rotacao',
+          'Distribuicao',
+          'Combustivel',
+          'Posicao',
+          'Choke')
+	df = pd.DataFrame(columns=col_labels)
 	
 	def __init__(self, PORT='COM6', BaudRate = 9600):
-		super().__init__(PORT,BaudRate)
+		
+		# atribui a tstart o tempo do sistema na criação do LeitorSerial
+		tstart = datetime.now()
+		
+		"""
+		# configura a conexão serial
+		# detalhes em "https://pythonhosted.org/pyserial/pyserial_api.html"
+		ser = serial.Serial(
+			port=PORT,
+			baudrate=BaudRate,
+			parity=serial.PARITY_NONE,
+			stopbits=serial.STOPBITS_ONE,
+			bytesize=serial.EIGHTBITS
+		)
+		"""
+		
+		print ('Iniciou Leitor Serial')
+		
+		# abre a conexão serial
+		#ser.open()
 	
 	#Aqui deve ser retornado o dicionário com as leitura feitas
 	def Leitura(self):
-		pass
+		readings = [np.float(x) for x in self.LeituraAleatoria().split(';')[:-1]]
+		time = (datetime.now()-self.tstart).total_seconds()
+		readings.insert(0, time)
+		self.df = self.df.append(dict(zip(self.col_labels, readings)), ignore_index=True)
+		sleep(0.1)
+		
+		
+	def LeituraAleatoria(self):
+		l = [str(x) for x in np.random.randint(0, 100, size=7)]
+		l += ['\n']
+		return ';'.join(l)
--- Código/main.py
+++ Código/main.py
@@ -1,16 +1,87 @@
 from LeitorSerial import LeitorSerial
 import Interpretadores
+import pandas as pd
+
+
+def salvar_excel(df):
+    # Salvar
+    #global VELOCIDADE
+    #global ROTACAO
+    #global tempo
+    #global DISTRIBUICAO
+    #global COMBUSTIVEL
+    #global Kmrodados
+    #global POSICAO
+    #global TempoM2
+    writer = pd.ExcelWriter('output.xlsx')
+    df.to_excel(writer,'Sheet1')
+    writer.save()
+    
 
 
 #Aqui ficará a função que será chamada para executar os códigos do projeto
 #Nela deve conter chamadas às funções de leitura, interpretação e depois exibição
 def main():
-    l = LeitorSerial()
-    pass
-
-
-
-
+    leitor = LeitorSerial()
+    for i in range(5):
+        leitor.Leitura()
+        
+        #Atribui a CHOKE o ultimo Choke registrado
+        CHOKE = leitor.df.Choke[len(leitor.df)-1]
+        if CHOKE == 1:
+            print('preto')
+            pass  #Tem que botar alguma coisa da interface em preto
+        elif CHOKE ==0:
+            print('vermelho')
+            pass  #Tem que botar alguma coisa da interface em vermelho
+        else:
+            print('Outra bosta')
+            print(CHOKE)
+        
+        #Trecho comentado até termos integração com frontend
+        ''' 
+        #Atribui a BotaoBOX o valor do Botão BOX
+        BotaoBOX = pegar na interface
+        
+        #Atribui a BOX o ultimo Box registrado
+        BOX = leitor.df.Box[len(leitor.df)-1]
+        if BotaoBOX == 1:
+            if BOX == 0 or BOX == 48:
+                leitor.MandaUm() #Função ainda não implementada para transmitir '1' na porta serial
+            elif BOX == 49:
+                print('vermelho')
+                pass  #Tem que botar alguma coisa da interface em vermelho
+        elif BotaoBOX == 0:
+            if BOX == 49:
+                leitor.MandaZero() #Função ainda não implementada para transmitir '0' na porta serial
+            elif BOX == 48:
+                print('preto')
+                pass  #Tem que botar alguma coisa da interface em preto
+        '''
+        
+        # Calculo de distancia percorrida
+        # Uma vez que é necessário o acesso aos dois últimos registros, 
+        # é preciso impedir que esse cálculo seja feito na primeira iteração
+        if i==0:
+            KmRodadosTotal = 0
+        else:
+            #Calcula o index da ultima leitura (se garantirmos um i será substituído)
+            IndexAtual = len(leitor.df)-1
+            #Retorna a velocidade média em m/s
+            VelMediaMPS = ((leitor.df.Velocidade[IndexAtual] + leitor.df.Velocidade[IndexAtual - 1])/2)/3.6
+            #Retorna os Km Rodados nesta iteração
+            KmRodadosAtual = VelMediaMPS * (leitor.df.Tempo[IndexAtual] - leitor.df.Tempo[IndexAtual-1])/1000
+            #Soma os Km Rodados nesta iteração ao total
+            KmRodadosTotal += KmRodadosAtual
+        
+        
+        '''
+        Aqui deve ficar as partes da interface
+        '''
+    
+    
+    print(leitor.df)
+    
 
 #Chama a função main se o script for executado como main e não faz nada se for chamado por outro script
 if __name__ == "__main__":
